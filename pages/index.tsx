693639ce97d720362c49e2a31516f11237dc784d
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2021-08-29 10:47:46 +0400

    project re-init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AirPods Pro - Apple</title>
+        <meta
+          name="description"
+          content="AirPods Pro. Active Noise Cancellation, Transparency mode, and a customizable fit — all in an incredibly light in-ear headphone."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
